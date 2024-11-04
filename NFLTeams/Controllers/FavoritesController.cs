b27e1951e81645a607824e16e9ebde4b022653ed
kazi-a <kazi.arafat@hotmail.com>
2024-11-03 21:55:58 -0500

    Add project files.

---
+++ NFLTeams/Controllers/FavoritesController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private TeamContext context;
+
+        public FavoritesController(TeamContext ctx) => context = ctx;
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            // Create an instance of NFLSession to access session data
+            var session = new NFLSession(HttpContext.Session);
+
+            // Gather user information and favorite teams from session
+            var model = new TeamsViewModel
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+                Teams = session.GetMyTeams(),
+                Username = session.GetName() // Add the username here
+            };
+
+            return View(model); // Return the populated view model to the view
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Add(Team team)
+        {
+            // Fetch the full team data from the database
+            team = context.Teams
+                 .Include(t => t.Conference)
+                 .Include(t => t.Division)
+                 .Where(t => t.TeamID == team.TeamID)
+                 .FirstOrDefault() ?? new Team();
+
+            // Add the team to the user's favorite teams in session
+            var session = new NFLSession(HttpContext.Session);
+            var teams = session.GetMyTeams();
+            teams.Add(team);
+            session.SetMyTeams(teams);
+
+            // Set a message to indicate the addition
+            TempData["message"] = $"{team.Name} added to your favorites";
+
+            // Redirect to the Home page
+            return RedirectToAction("Index", "Home",
+                new
+                {
+                    ActiveConf = session.GetActiveConf(),
+                    ActiveDiv = session.GetActiveDiv()
+                });
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Delete()
+        {
+            // Remove favorite teams from session
+            var session = new NFLSession(HttpContext.Session);
+            session.RemoveMyTeams();
+
+            // Set a message to indicate the clearance of favorites
+            TempData["message"] = "Favorite teams cleared";
+
+            // Redirect to the Home page
+            return RedirectToAction("Index", "Home",
+                new
+                {
+                    ActiveConf = session.GetActiveConf(),
+                    ActiveDiv = session.GetActiveDiv()
+                });
+        }
+    }
+}
---
+++ NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers
+{
+    public class NameController : Controller
+    {
+        private NFLSession _nflSession;
+
+        public NameController(NFLSession nflSession)
+        {
+            _nflSession = nflSession;
+        }
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var model = new TeamsViewModel
+            {
+                ActiveConf = _nflSession.GetActiveConf(),
+                ActiveDiv = _nflSession.GetActiveDiv(),
+                Teams = _nflSession.GetMyTeams(),
+                Username = _nflSession.GetName() 
+            };
+
+            return View(model);
+        }
+
+        // Change action method for POST requests
+        [HttpPost]
+        public RedirectToActionResult Change(TeamsViewModel model)
+        {
+            var session = new NFLSession(HttpContext.Session); 
+            session.SetName(model.Username); 
+
+            // Redirect to the Home page with current active conference and division
+            return RedirectToAction("Index", "Home", new
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv()
+            });
+        }
+    }
+}
